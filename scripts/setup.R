1b95348209a4d531c1270de7eed3f6227e1345a9
Myfanwy <mrowlan1@gmail.com>
2018-11-06 11:45:53 -0800

    add rproj to gitignore

---
+++ scripts/setup.R
@@ -0,0 +1,25 @@
+#--------------------------------------------#
+# setup
+#--------------------------------------------#
+
+library(fishtrackr)
+library(ybp)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+# detection windows for each year
+
+
+d <- all69khz_grouped %>% 
+  mutate(Detyear = case_when(
+    DateTimeUTC %within% (ymd_hms("2011-07-01 00:00:00") %--% ymd_hms("2012-06-30 23:59:59")) ~ 2011 ,
+    DateTimeUTC %within% (ymd_hms("2012-07-01 00:00:00") %--% ymd_hms("2013-06-30 23:59:59")) ~ 2012 ,
+    DateTimeUTC %within% (ymd_hms("2013-07-01 00:00:00") %--% ymd_hms("2014-06-30 23:59:59")) ~ 2013 ,
+    DateTimeUTC %within% (ymd_hms("2014-07-01 00:00:00") %--% ymd_hms("2015-06-30 23:59:59")) ~ 2014 ,
+    DateTimeUTC %within% (ymd_hms("2015-07-01 00:00:00") %--% ymd_hms("2016-06-30 23:59:59")) ~ 2015 ,
+    DateTimeUTC %within% (ymd_hms("2016-07-01 00:00:00") %--% ymd_hms("2017-06-30 23:59:59")) ~ 2016 ,
+    TRUE ~ 2017)) %>% 
+  select(-Frequency)
+
+wst <- filter(d, Sp == "wst")
+wst <- fishtrackr::fishpaths(wst, wst$TagID, wst$Station)

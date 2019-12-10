cabd8add76ddcf4979494492bcb6f02260071921
Myfanwy <mrowlan1@gmail.com>
2019-12-09 16:32:14 -0800

    fix first_last function call

--- scripts/chn_exits.R
+++ scripts/chn_exits.R
@@ -3,6 +3,36 @@
 source("scripts/setup.R")
 
 fl <- first_last(chn, "TagID", "DateTimeUTC", "Station")
+# detdf = chn
+# tagidcol = "TagID"
+# datetimecol = "DateTimeUTC"
+# stationcol = "Station"
+# 
+# detdf$Station = detdf[[stationcol]]
+# detdf$datetimecol = detdf[[datetimecol]]
+# detdf$TagID = detdf[[tagidcol]]
+# 
+# newdf <-  do.call(rbind, lapply(split(detdf, detdf$TagID), function(x) {
+# 
+#      x = x[order(x$datetimecol), ]
+# 
+#      return(data.frame(
+# 
+#        TagID = as.numeric(unique(x$TagID)),
+# 
+#        first_det = min(x$datetimecol),
+# 
+#        first_stn = x$Station[x$datetimecol == min(x$datetimecol)],
+# 
+#        last_det = max(x$datetimecol),
+# 
+#        last_stn = x$Station[x$datetimecol == max(x$datetimecol)],
+# 
+#        stringsAsFactors = FALSE)
+#      )
+#    }
+#   ))
+
 chnidx <- select(chn, TagID, TagGroup) %>% 
   filter(!duplicated(TagID))
 
@@ -11,7 +41,7 @@ fl <- left_join(fl, chnidx)
 fl <- fl %>% 
   filter(TagGroup != "fca_2012") %>%  #filter out 2012 fish; not included in analyses
   group_by(TagID) %>% 
-  mutate(ExitStatus = case_when(LastStation %in% exitstations ~ 1 , 
+  mutate(ExitStatus = case_when(last_stn %in% exitstations ~ 1 , 
                             TRUE ~ 0)) %>% 
   ungroup() %>% 
   mutate(Detyear = 
@@ -26,7 +56,40 @@ fl <- fl %>%
         Species = "chn") %>% 
   select(TagID, Species, Detyear, TagDetyear, ExitStatus)
 
+head(fl)
+
+sheds <- structure(list(TagID = c(13728, 13729, 20168, 20164, 2600, 2625, 
+2619, 9986, 9973), DateTimePST_truncated = c("2015-01-01 0:00:00", 
+"2014-12-04 15:23:06", "2014-11-16 21:26:10", "2014-12-23 4:04:55", 
+"2017-10-16 20:51:12", "2017-12-26 13:06:10", "2017-11-20 7:42:04", 
+"2018-12-03 6:37:13", "2018-12-02 10:30:08"), GroupedStn_truncated = c("BCN", 
+"YBR22", "YBALW", "YBAWW", "YBRSTR", "YBAAG4", "YBBLW", "YBAWW", 
+"YBBWW")), row.names = c(NA, -9L), class = "data.frame")
+
+table(sheds$GroupedStn_truncated)
+sheds
+
+recovs = c(9970, 37835, 37845, 2639, 2596, 2593, 2603, 33937, 45433, 45429, 
+45451, 31565, 13721, 33940)
+
+sum(sheds$TagID %in% recovs)
+intersect(sheds$TagID, recovs)
+
+recovfps <- filter(chn, TagID %in% recovs)
+
+recovfps %>% 
+  filter(TagID %in% c(33940, 37835,37845)) %>% 
+  ggplot(aes(x = DateTimePST, y = reorder(Station,Rkm))) +
+  geom_point(alpha = 0.5) +
+  facet_wrap(~TagID)
+
+flrecov <- first_last(recovfps)
+flrecov$ExitStatus <- ifelse(flrecov$last_stn %in% c("BC_joint2","BC_joint", "Base_TD"), 1, 2)
+flrecov$ExitStatus[flrecov$TagID == 37845] <- 1 # WW rescue
+
 chn_exits_final <- fl
+
+
 
 saveRDS(chn_exits_final, "data/chn_exits_final.rds")
 

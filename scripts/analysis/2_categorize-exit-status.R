0c411f31265c31d7f6471112445f41131754ff18
Myfanwy <mrowlan1@gmail.com>
2019-12-30 16:00:28 -0800

    updated wst munging to reflect tag end dates

--- scripts/analysis/2_categorize-exit-status.R
+++ scripts/analysis/2_categorize-exit-status.R
@@ -33,8 +33,53 @@ chn_dets = subset(dets8, TagID %in% chntags$TagID)
 chn_fl = first_last(chn_dets, "TagID", "DateTimePST", "GroupedStn")
 
 
+# with white sturgeon, have to go by year.  Goal: make a list of all wst tags and detection years
+wst_tags = readxl::read_excel("data_raw/tag_data_raw/WhiteSturgeon_tags.xlsx", na="") %>% 
+  select(DateTagged, TagID, CodeSpace, EstTagLife_days, TagGroup)
+
 wst_dets = subset(dets8, TagID %in% alltags$TagID[alltags$Sp == "wst"])
 
 wst_dets = get_det_year(wst_dets, "DateTimePST")
 
-# with white sturgeon, have to go by year.
+wst_exits = mk_exit_tbl()
+wst_tbl = mk_wst_tbl()
+
+wst_exits = merge(wst_exits, wst_tbl, all.x = TRUE, all.y = TRUE, by = "TagID")
+
+wst_tags = add_tag_end_col(wst_tags)
+wst_dets = left_join(wst_dets, select(wst_tags, TagID, EstTagEnd))
+wst_dets2 = rm_post_tag_end_dets(wst_dets) 
+stopifnot(nrow(wst_dets) - nrow(wst_dets2) == 68) # removed the 68 dets from Tag 2860
+
+wst_exits2 <- wst_exits %>% 
+  group_by(TagID) %>% 
+  filter(Detyear >= DetyearTagged) %>% 
+  ungroup() %>% 
+  arrange(TagID, Detyear) 
+
+#--------------------------------------------#
+
+# Make table of detections per fish per year
+dettable <- as.data.frame.matrix(table(wst$TagID, wst$Detyear))
+write.csv(dettable, file = "data/wst_returns.csv", quote = FALSE)
+
+# Check 2011 fish's last locations:
+wst_paths <- fishpaths(wst, wst$TagID, wst$Station)
+wst_paths <- wst_paths %>% 
+  arrange(TagID, DateTimeUTC) %>% 
+  select(TagID, DateTimeUTC, Station, Detyear, DateTagged)
+
+filter(wst_paths, TagID == 2841) # iterated through each tagID and verified that final detection was at BC_joint or Base_TD; if so, 1, if not, 0. Saved in wst_returns_handchecked.xlsx
+
+#--------------------------------------------#
+wst_returns_handchecked <- readxl::read_excel("data/wst_returns_handchecked.xlsx", na = "NA")
+
+wst_returns_hc_tidy <- tidyr::gather(wst_returns_handchecked, key = "Detyear", value = "ExitStatus", -TagID) %>% 
+  mutate(Detyear = as.integer(Detyear))
+
+wst_exits_final <- left_join(wst_exits, wst_returns_hc_tidy) %>% 
+  filter(!is.na(ExitStatus)) %>% 
+  mutate(Species = "wst") %>% 
+  select(TagID, Species, Detyear, TagDetyear, ExitStatus)
+
+saveRDS(wst_exits_final, "data/wst_exits_final.rds")
--- scripts/functions/categorizing_exits_functions.R
+++ scripts/functions/categorizing_exits_functions.R
@@ -1,42 +1,64 @@
 #-------------------------------------------------------#
 # M. Johnston
 # Functions for categorizing exit status 
-# Fri Dec 27 13:17:16 2019 ------------------------------
+# Mon Dec 30 09:49:44 2019 ------------------------------
+
+# wst tagids:
+mk_wst_tbl = function() {
+  wsttable = alltags %>% 
+  filter(Sp == "wst") %>% 
+  mutate(DetyearTagged = year(DateTagged) - 1,
+         YearTagged = year(DateTagged)) %>% 
+  select(TagID, YearTagged, DetyearTagged) %>% 
+  left_join(select(filter(wst_dets, !duplicated(TagID)), TagID)) 
+  return(wsttable)
+  }
+
+# make exit status dataframe to be filled in
+mk_exit_tbl = function() {
+  wsttags = alltags %>% filter(Sp == "wst") %>% pull(TagID)
+  detyear = c(2011:2017)
+  wst_exits = expand.grid(TagID = wsttags, Detyear = detyear, stringsAsFactors = FALSE)
+  return(wst_exits)
+}
 
 
-first_last <- function(detdf, tagidcol = "TagID", datetimecol = "DateTimeUTC", stationcol = "Station") {
+# calculate end tag date for each wst tag:
 
+add_tag_end_col = function(wst_tags_df) {
   
-  # test:
-  # detdf = chn_dets
-  # tagidcol = "TagID"
-  # datetimecol = "DateTimePST"
-  # stationcol = "GroupedStn"
-  f1 <- split(detdf, detdf[[tagidcol]])
-  tmp <- lapply(f1, first_last_1fish, dtc2 = datetimecol, stnc2 = stationcol)
-  fldf = do.call(rbind, tmp)
-                 
-  return(fldf) }
-                 
-first_last_1fish <- function(x, 
-                             dtc2 = datetimecol, 
-                             tagc = tagidcol,
-                             stnc2 = stationcol) {
+  wst_tags_dff = wst_tags_df %>% 
+    mutate(EstTagEnd = DateTagged + duration(EstTagLife_days, "days"))
+  
+  return(wst_tags_dff)
+  
+}
 
-     x = x[order(x[[dtc2]]), ] # order by DateTime
+# visualize tag life:
+wst_taglife_viz <- function(tagdf) {
+  
+pp = ggplot(tagdf, aes(x = DateTagged, y = factor(TagID))) +
+ geom_point(aes(color = factor(CodeSpace)), show.legend = FALSE) +
+ geom_segment(aes(x = DateTagged, xend = EstTagEnd, 
+                  y = factor(TagID), 
+                  yend = factor(TagID)),
+              size = 0.2) +
+  geom_point(aes(x = EstTagEnd, color = factor(CodeSpace)), show.legend = FALSE) +
+  theme_minimal() +
+  labs(x = "Tag life from Tagging Date to estimated tag life end",
+       y = "TagID",
+       subtitle = "colors indicate different codespaces")
 
-     return(data.frame(
+pp
+}
 
-       TagID = as.numeric(unique(x[[tagc]])),
-
-       first_det = min(x[[dtc2]]),
-
-       first_stn = x[[stnc2]][x[[dtc2]] == min(x[[dtc2]])],
-
-       last_det = max(x[[dtc2]]),
-
-       last_stn = x[[stnc2]][x[[dtc2]] == max(x[[dtc2]])],
-
-       stringsAsFactors = FALSE)
-     )
+rm_post_tag_end_dets <- function(dets_df){
+  dets_df %>% 
+    group_by(TagID) %>% 
+    arrange(DateTimePST) %>% 
+    filter(DateTimePST <= EstTagEnd) %>% 
+    ungroup() -> dets_dff
+  
+  return(dets_dff)
+  
 }
